eb7954d043c9bf5c29d7f0a5bceceb94065316af
Fungai Chiwarira <chiwarira@gmail.com>
2023-01-19 20:20:54 +0200

    Create a static web interface

--- todo/functions.py
+++ todo/functions.py
@@ -1,9 +1,9 @@
-def get_todos(filepath='todo/todos.txt'):
+def get_todos(filepath='todos.txt'):
     with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
 
-def write_todos(todos_args, filepath='todo/todos.txt'):
+def write_todos(todos_args, filepath='todos.txt'):
     with open(filepath, 'w') as file:
         file.writelines(todos_args)
